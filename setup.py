2c62bbcbfe280a18740cdd9d0e56c6c3f1b9819e
Deepak Singh Manola <deepak.singh.m@gmail.com>
2026-03-14 00:44:46 +0530

    Initial commit: Medical Chatbot with RAG

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name= "medical-chatbot",
     version= "0.1.0",
-    author= "Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author= "Deepak Singh Manola",
+    author_email="deepak.singh.m@gmail.com",
     packages= find_packages(),
     intstall_requires=[]
 )
